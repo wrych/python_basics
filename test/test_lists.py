5b59e3f52af58b035f87bb3c1352775089cbb263
Andy Moesch <andy.moesch@hotmail.com>
2020-06-29 21:48:27 +0200

    add some list basics

--- test/test_lists.py
+++ test/test_lists.py
@@ -23,3 +23,38 @@ def test_insert_into_list(index):
     x_1 = list_basics.insert_into_list(x, index, y)
     assert len(x_1) == len_x + 1
     assert x_1[index] == y
+
+
+@pytest.mark.parametrize("multiplier", [0, 1, 2])
+def test_multiply_elements(multiplier):
+    x = [1, 2, 3]
+    assert list_basics.multiply_elements(x, multiplier) == list(
+        map(lambda e: e * multiplier, x)
+    )
+
+
+def test_stringify_list():
+    x = [1, 2, 3]
+    template = "hello {}"
+    assert list_basics.stringify_list(x, template) == list(
+        map(lambda e: template.format(e), x)
+    )
+
+
+def test_get_list_of_ints():
+    x = ["a", 1, 2, "v", 3, "b"]
+    assert list_basics.get_list_of_ints(x) == [1, 2, 3]
+
+
+def test_multiply_and_format():
+    x = [2, 4, 5]
+    template = "banana {} joe"
+    assert list_basics.multiply_and_format(x, 2, template) == list(
+        map(lambda e: template.format(e * 2), x)
+    )
+
+
+def test_format_strings_in_list():
+    x = ["banana {}", "joe {}", "sallee"]
+    y = "xxx"
+    assert list_basics.format_strings_in_list(x, y) == ["banana xxx", "joe xxx"]
