3f28e97690583676ced6e82afdef58e2229ef503
Smaillim Paz <65830045+smaillimp@users.noreply.github.com>
2020-07-27 19:22:02 +0200

    Print Hello World!

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -4,3 +4,6 @@ to the stdout when it is called.
 It should not print "Hello World!" if it is imported.
 Write a test that goes with it.
 """
+
+if __name__ == "__main__":    
+    print('Hello World!')
