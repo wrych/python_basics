853d3089ed55a6bb1f620d58127af6cdffa5d306
Smaillim Paz <65830045+smaillimp@users.noreply.github.com>
2020-07-27 22:37:08 +0200

    calculator

--- scripts/multiply_by_2.py
+++ scripts/multiply_by_2.py
@@ -1,3 +1,9 @@
 """
 Write a script that asks for an input and multiplies it by 2.
 """
+if __name__ == "__main__":
+    print(multiply_by_2(int(input('Please write a number, I am going to make it double! xD : '))))
+
+
+def multiply_by_2(val):
+    return val*2
