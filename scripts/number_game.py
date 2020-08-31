2102a11d4aecf17dd3ea008e8fd0ce7f45a565f2
Andy Moesch <andy.moesch@hotmail.com>
2020-08-31 20:06:00 +0200

    add random output to number game

--- scripts/number_game.py
+++ scripts/number_game.py
@@ -1,32 +1,40 @@
-'''
+"""
 Write a program that plays a game of numbers. 
 The first input is going to be the magic number. 
 After that, the players need to count upwards (i.e. 1, 2, 3 etc.). 
 Whenever the number is divisible or has the magic number in it, the player needs to write either “muh” or “meh”. 
 If the the player gives the wrong number or a number when he should be typing “muuh” or “meh”, exit and print “you’re a loser”.
-'''
+"""
+
+import random
+
+
+answers_list = [
+    "A cow counts better than you, tha is why You're a loser",
+    "You lost count and You're a loser now",
+]
+
+
+def printing_random_answer():
+    print(random.choice(answers_list))
 
 
 if __name__ == "__main__":
     count = 0
-    magic_number = int(input('Please, Choose and write the magic number: \n'))
- 
+    magic_number = int(input("Please, Choose and write the magic number: \n"))
+
     while True:
         count = count + 1
         users_number = input("Write the number or do like a cow if required: \n")
         if count % magic_number == 0:
             if users_number not in ["muuh", "meeh"]:
-                print("A cow counts better than you, tha is why You're a loser")
+                printing_random_answer()
                 break
         elif str(magic_number) in str(count):
             if users_number not in ["muuh", "meeh"]:
-                print("A cow counts better than you, tha is why You're a loser")
+                printing_random_answer()
                 break
         elif int(users_number) != count:
-            print("You lost count and You're a loser now")
+            printing_random_answer()
             break
-
-
-    
-
 
