98e5e9eb180fb30e0b5be5c2d063ca94548f5ed5
Smaillim Paz <65830045+smaillimp@users.noreply.github.com>
2020-09-04 18:41:52 +0200

    printing the last element

--- scripts/calculate_primes.py
+++ scripts/calculate_primes.py
@@ -1,22 +1,20 @@
-import sys
 
+prime_numbers = []
+top_number = int(input('Please write the top value: '))
 
-def print_all_primes_under(max_value):
-    pass
+def checking_prime():
+    for num in range(top_number + 1):
+        if num > 1:
+            for n in range(2, num):
+                if num % n == 0:
+                    break
+            else:
+                prime_numbers.append(num)
+                print(prime_numbers[-1])        
 
+def main():  
+    checking_prime()
+    
 
-def checking_primes(input_number):
-    for n in range(2, input_number):
-        if input_number % n == 0:
-            return False
-    else:
-        return True
-
-
-def main():
-    max_value = sys.argv[1]
-    print_all_primes_under(max_value)
-
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
